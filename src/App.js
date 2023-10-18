d3a8c54c12ab7fb90979916771d698760f294e8b
Mohamed Amine Mahiddine <aminemahiddine.io@gmail.com>
2023-10-18 15:49:37 +0100

    push for producation

--- src/App.js
+++ src/App.js
@@ -7,31 +7,34 @@ import {
   Typography,
   Button,
   TextField,
-  // Box,
   IconButton,
   CssBaseline,
   Paper,
   ThemeProvider,
   createTheme,
   Switch,
+  Slider,
 } from '@mui/material';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SaveIcon from '@mui/icons-material/Save';
 
-
 const API_TOKEN = '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';
 
-// Create a dark theme
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
-
+const scrollableDivStyle = {
+  maxHeight: '300px', // You can adjust the height as needed
+  overflowY: 'auto',
+};
 const App = () => {
   const [text, setText] = useState('');
   const [summarizedText, setSummarizedText] = useState('');
+  const [minLength, setMinLength] = useState(0);
+  const [maxLength, setMaxLength] = useState(500);
   const [loading, setLoading] = useState(false);
   const [wordCount, setWordCount] = useState(0);
   const [summarizedWordCount, setSummarizedWordCount] = useState(0);
@@ -46,6 +49,14 @@ const App = () => {
       setCachedSummaries(JSON.parse(cachedSummariesJSON));
     }
   }, []);
+
+  const handleMinLengthChange = (event, newValue) => {
+    setMinLength(newValue);
+  };
+
+  const handleMaxLengthChange = (event, newValue) => {
+    setMaxLength(newValue);
+  };
 
   const handleTextChange = (e) => {
     const inputText = e.target.value;
@@ -105,8 +116,9 @@ const App = () => {
     try {
       const data = {
         inputs: text,
-        options: {
-          model: 'facebook/bart-large-cnn', // You can change the model here
+        parameters: {
+          min_length: minLength, // Set min_length
+          max_length: maxLength, // Set max_length
         },
       };
 
@@ -123,7 +135,6 @@ const App = () => {
           const summarizedWords = result[0].summary_text.split(/\s+/).filter((word) => word !== '');
           setSummarizedWordCount(summarizedWords.length);
 
-          // Cache the summary
           setCachedSummaries({ ...cachedSummaries, [text]: result[0].summary_text });
           localStorage.setItem('cachedSummaries', JSON.stringify({ ...cachedSummaries, [text]: result[0].summary_text }));
         } else {
@@ -144,10 +155,10 @@ const App = () => {
     <ThemeProvider theme={darkMode ? darkTheme : createTheme()}>
       <CssBaseline />
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <Container maxWidth="md">
+        <Container maxWidth="lg">
           <Grid container spacing={2}>
             <Grid item xs={12} md={6}>
-              <Card elevation={3} style={{ display: 'flex', flexDirection: 'column', height: '100%', padding: '20px' }}>
+              <Card elevation={12} style={{ display: 'flex', flexDirection: 'column', height: '100%', padding: '20px' }}>
                 <CardContent style={{ flexGrow: 1 }}>
                   <Typography variant="h5" style={{ marginBottom: '20px' }}>Text before summarizing</Typography>
                   <TextField
@@ -158,19 +169,42 @@ const App = () => {
                     value={text}
                     onChange={handleTextChange}
                   />
+                  <br />
                   <Typography>Word Count: {wordCount}</Typography>
                   {error && <Typography variant="body1" style={{ color: 'red' }}>{error}</Typography>}
+                </CardContent>
+                <CardContent>
+                  <Typography variant="h6" style={{ marginBottom: '10px' }}>Summary Length</Typography>
+                  <div>Min</div>
+                  <Slider
+                    value={minLength}
+                    min={0}
+                    max={maxLength - 1}
+                    onChange={handleMinLengthChange}
+                    valueLabelDisplay="auto"
+                  />
+                  <div>Max</div>
+                  <Slider
+                    value={maxLength}
+                    min={minLength + 1}
+                    max={1000} // You can adjust the max value as needed
+                    onChange={handleMaxLengthChange}
+                    valueLabelDisplay="auto"
+                  />
                 </CardContent>
               </Card>
             </Grid>
             <Grid item xs={12} md={6}>
-              <Card elevation={3} style={{ display: 'flex', flexDirection: 'column', height: '100%', padding: '20px' }}>
+              <Card elevation={12} style={{ display: 'flex', flexDirection: 'column', height: '100%', padding: '20px' }}>
                 <CardContent style={{ flexGrow: 1 }}>
                   <Typography variant="h5" style={{ marginBottom: '20px' }}>Text after summarizing</Typography>
                   {summarizedText && (
                     <>
                       {cachedSummaryMessage && <Typography variant="body2" style={{ color: 'green' }}>{cachedSummaryMessage}</Typography>}
+                      <div style={scrollableDivStyle}>
                       <Typography variant="body1">{summarizedText}</Typography>
+                      </div>
+                      <br />
                       <Typography>Word Count (Summarized Text): {summarizedWordCount}</Typography>
                     </>
                   )}
